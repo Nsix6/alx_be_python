007e7785e394ff5005aa37666222f67db26bff79
Nsix6 <nsikakmbre@gmail.com>
2025-09-25 17:01:36 +0100

    date_time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,11 @@
+from datetime import datetime, date, time, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(formatted)
+
+def calculate_future_date(days):
+    today = date.today()
+    future_date = today + timedelta(days=days)
+    return future_date
