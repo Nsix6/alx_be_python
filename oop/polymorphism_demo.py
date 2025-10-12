786251235c4d7b79d1e630e6bd83a4ab764b59ed
Nsix6 <nsikakmbre@gmail.com>
2025-10-12 18:10:11 +0100

    polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,26 @@
+class Shape:
+    """Base class for different shapes."""
+
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method")
+    
+class Rectangle(Shape):
+    """A rectangle shape."""
+    
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+    
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.width * self.length
+    
+class Circle(Shape):
+    """A circle shape."""
+    
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        """Calculate the area of the circle."""
+        return 3.14159 * (self.radius ** 2)
